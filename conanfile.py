efdedb552fa6a9e1448620313f2ebdc813ed1f27
werto87 <huhulasmichinruhe@web.de>
2024-02-02 16:03:25 +0900

    new cmake layout. conan2

--- conanfile.py
+++ conanfile.py
@@ -1,28 +1,14 @@
-from conans import ConanFile, CMake
+from conan import ConanFile
 
 
-class login_matchmaking_game_shared_type(ConanFile):
-    name = "login_matchmaking_game_shared_type"
-    license = "<Put the package license here>"
-    url = "<Package recipe repository url here, for issues about the package>"
-    description = "<Description of hello here>"
+class Project(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = {"shared": False}
-    generators = "cmake"
-    exports_sources = "*"
+    generators = "CMakeToolchain", "CMakeDeps"
+
+    def configure(self):
+        self.options["catch2"].with_main = True
+        self.options["catch2"].with_benchmark = True
 
     def requirements(self):
-        self.requires("boost/1.81.0")
-
-    def build(self):
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
-
-    def package(self):
-        self.copy(pattern="*", dst="include/login_matchmaking_game_shared_type",
-                  src="login_matchmaking_game_shared_type")
-
-    def package_id(self):
-        self.info.header_only()
+        self.requires("durak/1.0.0")
+        self.requires("boost/1.84.0")
--- test/main.cxx
+++ test/main.cxx
@@ -1,3 +1,8 @@
 #include "login_matchmaking_game_shared_type/matchmakingGameSerialization.hxx"
 #include "login_matchmaking_game_shared_type/userMatchmakingSerialization.hxx"
 // Use some types to check if the file compiles
+
+//here to test if the library compiles
+int main(){
+  return 0;
+}
