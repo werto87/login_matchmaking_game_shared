ed522d84371515ebfbcd5fd5c526863f394b01eb
werto87 <huhulasmichinruhe@web.de>
2024-06-03 17:37:54 +0200

    adapt struct GameOptionBase

--- login_matchmaking_game_shared/gameOptionBase.hxx
+++ login_matchmaking_game_shared/gameOptionBase.hxx
@@ -1,4 +1,32 @@
 #pragma once
+#include <boost/algorithm/string.hpp>
+#include <boost/fusion/adapted/struct/adapt_struct.hpp>
+#include <boost/fusion/adapted/struct/define_struct.hpp>
+#include <boost/fusion/algorithm/query/count.hpp>
+#include <boost/fusion/functional.hpp>
+#include <boost/fusion/include/adapt_struct.hpp>
+#include <boost/fusion/include/algorithm.hpp>
+#include <boost/fusion/include/at.hpp>
+#include <boost/fusion/include/count.hpp>
+#include <boost/fusion/include/define_struct.hpp>
+#include <boost/fusion/sequence/intrinsic/at.hpp>
+#include <boost/fusion/sequence/intrinsic_fwd.hpp>
+#include <boost/hana/assert.hpp>
+#include <boost/hana/at_key.hpp>
+#include <boost/hana/equal.hpp>
+#include <boost/hana/find.hpp>
+#include <boost/hana/for_each.hpp>
+#include <boost/hana/integral_constant.hpp>
+#include <boost/hana/map.hpp>
+#include <boost/hana/optional.hpp>
+#include <boost/hana/pair.hpp>
+#include <boost/hana/tuple.hpp>
+#include <boost/hana/type.hpp>
+#include <boost/json.hpp>
+#include <boost/mpl/for_each.hpp>
+#include <boost/mpl/if.hpp>
+#include <boost/mpl/range_c.hpp>
+
 namespace user_matchmaking_game
 {
 class GameOptionBase
@@ -7,3 +35,4 @@ public:
   virtual ~GameOptionBase () noexcept = default;
 };
 }
+BOOST_FUSION_ADAPT_STRUCT (user_matchmaking_game::GameOptionBase, );
--- login_matchmaking_game_shared/matchmakingGameSerialization.hxx
+++ login_matchmaking_game_shared/matchmakingGameSerialization.hxx
@@ -2,33 +2,7 @@
 #define F7525A03_A98E_4EEF_964B_E02274116B7D
 
 #include "login_matchmaking_game_shared/gameOptionBase.hxx"
-#include <boost/algorithm/string.hpp>
-#include <boost/fusion/adapted/struct/adapt_struct.hpp>
-#include <boost/fusion/adapted/struct/define_struct.hpp>
-#include <boost/fusion/algorithm/query/count.hpp>
-#include <boost/fusion/functional.hpp>
-#include <boost/fusion/include/adapt_struct.hpp>
-#include <boost/fusion/include/algorithm.hpp>
-#include <boost/fusion/include/at.hpp>
-#include <boost/fusion/include/count.hpp>
-#include <boost/fusion/include/define_struct.hpp>
-#include <boost/fusion/sequence/intrinsic/at.hpp>
-#include <boost/fusion/sequence/intrinsic_fwd.hpp>
-#include <boost/hana/assert.hpp>
-#include <boost/hana/at_key.hpp>
-#include <boost/hana/equal.hpp>
-#include <boost/hana/find.hpp>
-#include <boost/hana/for_each.hpp>
-#include <boost/hana/integral_constant.hpp>
-#include <boost/hana/map.hpp>
-#include <boost/hana/optional.hpp>
-#include <boost/hana/pair.hpp>
-#include <boost/hana/tuple.hpp>
-#include <boost/hana/type.hpp>
-#include <boost/json.hpp>
-#include <boost/mpl/for_each.hpp>
-#include <boost/mpl/if.hpp>
-#include <boost/mpl/range_c.hpp>
+
 #include <cstddef>
 #include <iostream>
 #include <map>
