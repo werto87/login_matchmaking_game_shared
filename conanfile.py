270c3d61385e313b36f7e86d3da801e04903b1a5
werto87 <huhulasmichinruhe@web.de>
2024-07-28 21:06:40 +0200

    version bump

--- conanfile.py
+++ conanfile.py
@@ -16,5 +16,5 @@ class Project(ConanFile):
         self.options["catch2"].with_benchmark = True
 
     def requirements(self):
-        self.requires("durak/1.0.5")
+        self.requires("durak/1.1.0")
         self.requires("boost/1.85.0")
