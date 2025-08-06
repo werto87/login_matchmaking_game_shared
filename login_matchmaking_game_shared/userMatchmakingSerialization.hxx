887ba0638bce495d29ee36972a1a7fffdcee60be
werto87 <huhulasmichinruhe@web.de>
2025-08-06 19:22:29 +0200

    disabled warnings as errors. added custom event support

--- login_matchmaking_game_shared/userMatchmakingSerialization.hxx
+++ login_matchmaking_game_shared/userMatchmakingSerialization.hxx
@@ -82,4 +82,5 @@ BOOST_FUSION_DEFINE_STRUCT ((user_matchmaking), GetLoggedInPlayers, (uint64_t, p
 BOOST_FUSION_DEFINE_STRUCT ((user_matchmaking), LoggedInPlayers, (std::vector<std::string>, players))
 BOOST_FUSION_DEFINE_STRUCT ((user_matchmaking), SubscribeGetLoggedInPlayers, (uint64_t, playerCount))
 BOOST_FUSION_DEFINE_STRUCT ((user_matchmaking), UnSubscribeGetLoggedInPlayers, )
+BOOST_FUSION_DEFINE_STRUCT ((user_matchmaking), CustomMessage, (std::string, message))
 #endif /* C02CDB99_AA83_45B0_83E7_8C8BC254A8A2 */
